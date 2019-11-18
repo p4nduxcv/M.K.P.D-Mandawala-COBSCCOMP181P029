f1a12258af852301eec49b0602c50d023b81f913
Pandula Mandawala <panduxcv@Pandulas-Mac.local>
2019-11-19 01:50:16 +0530

    layout desingns of login,signup,home

---
+++ M.K.P.D-Mandawala-COBSCCOMP181P029/View Controllers/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  M.K.P.D-Mandawala-COBSCCOMP181P029
+//
+//  Created by Pandula Mandawala on 11/18/19.
+//  Copyright © 2019 Pandula Mandawala. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
