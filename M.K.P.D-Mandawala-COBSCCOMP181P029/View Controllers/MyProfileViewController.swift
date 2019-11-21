d9f6d50b9a877f475864b59035d901162329bd16
Pandula Mandawala <panduxcv@Pandulas-Mac.local>
2019-11-22 02:58:00 +0530

    My Profile Page done

--- M.K.P.D-Mandawala-COBSCCOMP181P029/View Controllers/MyProfileViewController.swift
+++ M.K.P.D-Mandawala-COBSCCOMP181P029/View Controllers/MyProfileViewController.swift
@@ -7,13 +7,27 @@
 //
 
 import UIKit
+import Firebase
+
 
 class MyProfileViewController: UIViewController {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    @IBOutlet weak var mobileNumberLabel: UILabel!
+    @IBOutlet weak var fetchButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupElement()
+    }
+    
+    func setupElement(){
+        errorLabel.alpha=0
     }
     
 
@@ -27,4 +41,56 @@ class MyProfileViewController: UIViewController {
     }
     */
 
+    @IBAction func tappedFetchButton(_ sender: Any) {
+        
+        let db = Firestore.firestore()
+        
+        let user = Auth.auth().currentUser
+        if let user = user {
+            let uid = user.uid
+            db.collection("users").whereField("uid", isEqualTo: uid)
+                .getDocuments() { (querySnapshot, err) in
+                    if let err = err {
+                        print("Error getting documents: \(err)")
+                    } else {
+                        for document in querySnapshot!.documents {
+                         //print("\(document.documentID) => \(document.data())")
+                            let firstName = document.get("first_name")
+                            let lastName = document.get("last_name")
+                            let mobileNumber = document.get("mobile_number")
+                            let profilePhoto = document.get("profile_photo")
+                            
+                            self.firstNameLabel.text = firstName as? String
+                            self.lastNameLabel.text = lastName as? String
+                            self.mobileNumberLabel.text = mobileNumber as? String
+                            
+                            if let url = NSURL (string: profilePhoto as! String) {
+                                if let data = NSData(contentsOf: url as URL) {
+                                    self.profileImageView.contentMode = UIView.ContentMode.scaleAspectFit
+                                    self.profileImageView.image = UIImage (data: data as Data)
+                                    
+                                }
+                            }
+                        }
+                        
+                    }
+            }
+        }
+        //db.collectionGroup("users").whereField("uid", isEqualTo: true)
+        
+//        if currentUser == nil {
+//            showError("no users")
+//        } else {
+//
+//            db.collection("users").document(currentUser)
+//        }
+        
+        
+        
+    }
+    
+    func showError(_ message:String){
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
 }
